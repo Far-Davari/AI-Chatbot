b718421c947f00e7544786006fc9fb130861158c
Farshad Davari <DavariFarshad3@outlook.com>
2025-12-06 20:50:05 +0330

    Add functionality to send message btn.

---
+++ script.js
@@ -0,0 +1,44 @@
+const chatBody = document.querySelector(".chat-body");
+const messageInput = document.querySelector(".message-input");
+const sendMessgeButton = document.querySelector("#send-message");
+
+const userData = {
+  message: null,
+};
+
+// Create message element with dynamic classes and return it.
+const createMessageElement = (content, classes) => {
+  const div = document.createElement("div");
+  div.classList.add("message", classes);
+  div.innerHTML = content;
+  return div;
+};
+
+// Handle outgoing user messages
+const handleOutgoingMessage = (e) => {
+  e.preventDefault();
+  userData.message = messageInput.value.trim();
+  messageInput.value = "";
+
+  // Create and display user message
+  const messageContent = `<div class="message-text"></div>`;
+  const outgoingMessageDiv = createMessageElement(
+    messageContent,
+    "user-message"
+  );
+  outgoingMessageDiv.querySelector(".message-text").innerText =
+    userData.message;
+  chatBody.appendChild(outgoingMessageDiv);
+};
+
+// Handle Enter key press for sending messages
+messageInput.addEventListener("keydown", (e) => {
+  const userMessage = e.target.value.trim();
+  if (e.key === "Enter" && userMessage) {
+    handleOutgoingMessage(e);
+  }
+});
+
+sendMessgeButton.addEventListener("click", (e) => handleOutgoingMessage(e));
+
+// 29:20
